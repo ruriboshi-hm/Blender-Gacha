aede46a4db7e47cf2da621167e20bf47199eeb61
ruriboshi-hm <255476211+ruriboshi-hm@users.noreply.github.com>
2026-04-03 16:07:09 +0900

    3Dプリンター作品を追加、作品の説明文を追加

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ import random
 app = Flask(__name__)
 
 gacha = [
-    {"rarity":"SSR", "name":"将棋盤", "about":"説明文", "image":"/static/images/将棋盤.png", "weight":3},
+    {"rarity":"SSR", "name":"将棋盤", "about":"駒、盤、駒台まで全て制作。駒の角度と盛上駒の質感表現にこだわった。", "image":["/static/images/shogi.png"], "weight":3},
 
-    {"rarity":"SR", "name":"麻雀牌", "about":"説明文", "image":"/static/images/白ポン.png", "weight":9},
-    {"rarity":"SR", "name":"姿見", "about":"説明文", "image":"/static/images/鏡.png", "weight":8},
+    {"rarity":"SR", "name":"麻雀牌", "about":"牌のプラスチック感を意識してマテリアルを調整した。", "image":["/static/images/mahjong.png"], "weight":6},
+    {"rarity":"SR", "name":"姿見", "about":"家にある姿見を見ながら作った。画像だと分かりにくいが鏡部分は鏡面反射している。", "image":["/static/images/mirror.png"], "weight":5},
+    {"rarity":"SR", "name":"ソファ", "about":"1/50スケールで制作。3Dプリンターで印刷して建築模型に使用している。丁度いい座面の厚みにこだわった。", "image":["/static/images/sofa-print.jpg","/static/images/sofa.png"], "weight":6},
 
-    {"rarity":"R", "name":"カプセル剤", "about":"説明文", "image":"/static/images/medicine.png", "weight":27},
-    {"rarity":"R", "name":"マグカップ", "about":"説明文", "image":"/static/images/無題.png", "weight":27},
-    {"rarity":"R", "name":"ドーナツ", "about":"説明文", "image":"/static/images/ドーナツ.png", "weight":26}
+    {"rarity":"R", "name":"カプセル剤", "about":"チュートリアル動画を見ながら制作。カプセル部分と中身の比率調整が難しかった。", "image":["/static/images/medicine.png"], "weight":27},
+    {"rarity":"R", "name":"マグカップ", "about":"教材を見ながら制作。内側のマテリアルだけメタリックにして、保温できそうな雰囲気を作った。", "image":["/static/images/cup.png"], "weight":27},
+    {"rarity":"R", "name":"ドーナツ", "about":"ドーナツ生地の質感表現が難しかった。チョコのテカテカ感が気に入っている。", "image":["/static/images/doughnut.png"], "weight":26}
 ]
 
 @app.route("/")
--- static/script.js
+++ static/script.js
@@ -1,6 +1,8 @@
 const singleButton = document.getElementById("singleButton");
 const tenButton = document.getElementById("tenButton");
 const cardArea = document.getElementById("cardArea");
+const modal = document.getElementById("imageModal");
+const modalImage = document.getElementById("modalImage");
 
 async function drawGacha(count) {
     
@@ -16,12 +18,51 @@ async function drawGacha(count) {
 
         card.innerHTML = `
         <div class="card-inner">
-            <div class="card-front"><img src="/static/images/card-front.jpg" /></div>
-            <div class="card-back ${item.rarity}"><img src=${item.image} /><h3>${item.rarity} ${item.name}</h3>${item.about}</div>
+            <div class="card-front">
+                <img src="/static/images/card-front.jpg" />
+            </div>
+
+            <div class="card-back ${item.rarity}">
+                <div class="carousel">
+                    <img src=${item.image[0]} class="card-image"/>
+
+                    <div class="buttons">
+                        <button class="prev">◀</button>
+                        <button class="next">▶</button>
+                    </div>
+                </div>
+
+                <h3>${item.rarity} ${item.name}</h3>
+                <p>${item.about}</p>
+            </div>
         </div>
         `;
 
         cardArea.appendChild(card);
+
+        card.querySelector(".next").addEventListener("click",(e)=>{
+            e.stopPropagation();
+
+            let img = card.querySelector(".card-image");
+            let index = parseInt(img.dataset.index || 0);
+
+            index = (index + 1) % item.image.length;
+
+            img.src = `${item.image[index]}`;
+            img.dataset.index = index;
+        });
+
+        card.querySelector(".prev").addEventListener("click",(e)=>{
+            e.stopPropagation();
+
+            let img = card.querySelector(".card-image");
+            let index = parseInt(img.dataset.index || 0);
+
+            index = (index - 1 + item.image.length) % item.image.length;
+
+            img.src = `${item.image[index]}`;
+            img.dataset.index = index;
+        });
 
         setTimeout(()=>{
             card.classList.add("flipped");
@@ -31,3 +72,14 @@ async function drawGacha(count) {
 
 singleButton.addEventListener("click",()=>drawGacha(1))
 tenButton.addEventListener("click",()=>drawGacha(10));
+
+document.addEventListener("click", (e)=>{
+    if (e.target.classList.contains("card-image")){
+        modalImage.src = e.target.src;
+        modal.style.display = "flex";
+    }
+});
+
+modal.addEventListener("click",()=>{
+    modal.style.display = "none";
+});
